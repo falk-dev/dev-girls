6bb5a2a677aa3750d1dab2442feafdebd044ec7f
Falk Dev <mychelleketlen04@gmail.com>
2024-06-22 15:44:16 -0500

    Aula 01 - Introdução

---
+++ Aula 01 - Introdução/codigo1.cpp
@@ -0,0 +1,16 @@
+// Todas as bibliotecas estão inclusas
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int num;
+    num = 10;
+
+    cout << num << endl;
+
+    num = 23;
+
+    cout << "Agora a variavel num equivale a: " << num << endl;
+
+}
---
+++ Aula 01 - Introdução/codigo2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int num;
+
+    // Comando de entrada 
+    cin >> num;
+
+    // Comando de saída
+    cout << num << endl;
+
+    return 0;
+}
