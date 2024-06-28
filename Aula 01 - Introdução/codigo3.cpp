08d01d016cb9d7707e751305a6e4619f067e077a
Falk Dev <mychelleketlen04@gmail.com>
2024-06-28 09:58:04 -0500

    Aula 01 - Introdução

---
+++ Aula 01 - Introdução/codigo3.cpp
@@ -0,0 +1,25 @@
+// Todas as bibliotecas do c++
+#include <bits/stdc++.h>
+
+
+// Faz com que não seja necessário adicionar 'std' nos comandos
+using namespace std;
+
+// Executa tudo o que tá aqui dentro, função principal
+int main() {
+
+    // Declarando variável do tipo num
+    int num;
+
+    // Mostrando um texto para o usuário
+    cout << "Digite um numero: ";
+
+    // Guardando o que o usuário digitou na variável 'num'
+    cin >> num;
+
+    // Criando a variável resultado e somando o valor que o usuário digitou em 10 unidades
+    int resultado = num + 10;
+
+    // Imprimindo a variável resultado
+    cout << resultado;
+}
